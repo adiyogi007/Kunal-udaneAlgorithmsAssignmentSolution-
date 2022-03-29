e5293f18e620e8385b4e8d32824ff05f8e5c8eea
Admin <Admin@Purva>
2022-03-29 13:15:33 +0530

    assigment2

---
+++ assigment2/src/com/gl/dsa/assignment2/service/additionalservices.java
@@ -0,0 +1,25 @@
+package com.gl.dsa.assignment2.service;
+
+public class AdditionalServices 
+{
+      public void displayPricesAscendingOrder(double[] array)
+      {
+		System.out.println("Stock prices in ascending order are :");
+		for(int i=0;i<array.length;i++)
+		{
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+	}
+	public void displayPricesDescendingOrder(double[] array) 
+	{
+		System.out.println("Stock prices in descending order are :");
+		for(int i=array.length-1;i>=0;i--) 
+		{
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+	}
+
+
+}
---
+++ assigment2/src/comgldsaassignment2service/linearsearch.java
@@ -0,0 +1,15 @@
+package com.gl.dsa.assignment2.service;
+
+public class LinearSearch 
+{
+   public int countCompaniesInProfit(boolean array[]) 
+   {
+			int count =0;
+			for(int i=0;i<array.length;i++)
+			{
+				if(array[i]==true)
+					count++;
+			}
+			return count;
+	}
+}
